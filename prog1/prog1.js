9db2fa57f0dca05dda50d49557a95dcaeea94f34
Amanda <amandahelenaalfaro@gmail.com>
2016-09-13 10:41:35 -0500

    Add files via upload

--- prog1/prog1.js
+++ prog1/prog1.js
@@ -1,18 +1,15 @@
-var canvas
-var gl
+var canvas	//initializing the canvas
+var gl      //initializing gl
 
 
-function flatten(a){
-	return a.reduce(function(b,v){
-		b.push.apply(b, v);
-		return b}, [])
+function flatten(a)	{
+	return a.reduce(function (b,v) { b.push.apply(b, v); return b }, [])
 }
 
-function init(){
-
+function init()	{
 	canvas = document.getElementById('webgl');
 	gl = getWebGLContext(canvas, false);
-
+	
 	/*Initialize the program objects*/
 	var vertexSource = document.getElementById('vertexShader').text;
 	var fragmentSource = document.getElementById('fragmentShader').text;
@@ -20,35 +17,30 @@ function init(){
 	gl.useProgram(program);
 
 	var positions = [
-	//0
-	[0.0, 0.8, 0.0], 
+		//0
+		[   0.0 ,  0.8,  0.0  ], 
 
-	//1
-    [-0.4, 0.0, 0.0], 
+		//1
+	    [  -0.45,  0.0,  0.0  ], 
 
-	//2
-    [0.4, 0.0, 0.0], 
+		//2
+	    [   0.45,  0.0,  0.0  ], 
 
-    //3
-    [-0.8, -0.8, 0.0],
+	    //3
+	    [  -0.9,  -0.8,  0.0  ],
 
-    //4 
-    [0.0, -0.8, 0.0],
+	    //4 
+	    [   0.0,  -0.8,  0.0  ],
 
-    //5
-    [0.8, -0.8, 0.0]
-
-];
+	    //5
+	    [   0.9,  -0.8,  0.0  ]
+	];
 
 	var triangles = [
-
-    [ 0, 1, 2],
-
-    [1, 3, 4],
-
-    [2, 4 ,5]
-
-];
+	    [   0,     1,    2],
+	    [   1,     3,    4],
+	    [   2,     4 ,   5]
+	];
 
 	positionBuffer = gl.createBuffer();
 	triangleBuffer = gl.createBuffer();
@@ -63,14 +55,11 @@ function init(){
 	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, triangleBuffer);
 	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, triangleArray, gl.STATIC_DRAW);
 
-
 	requestAnimationFrame(draw);
-
 }
 
-function draw(){
-
-	gl.clearColor(0.0, 0.8, 0.0, 1.0);
+function draw()	{
+	gl.clearColor(0.0, 0.49, 0.0, 1.0);
 	gl.clear(gl.COLOR_BUFFER_BIT);
 
 	gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
@@ -84,7 +73,6 @@ function draw(){
 
 	/*Draw the triangle elements*/
 	gl.drawElements(gl.TRIANGLES, triangleArray.length, gl.UNSIGNED_SHORT, 0);
-
 }
 
 
